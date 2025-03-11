854330357e43ccb2ea96e0c3418a0c2a472ddde8
Nils Nerhus Rørstad <nnrorstad@gmail.com>
2025-03-11 14:26:11 +0100

    Lets see what happens

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' National Adaptation Plans Data
+#'
+#' A dataset containing information and text content from National Adaptation Plans
+#' (NAPs) downloaded from the UNFCCC NAP Central website. This dataset is automatically
+#' updated monthly through GitHub Actions.
+#'
+#' @format A tibble with the following variables:
+#' \describe{
+#'   \item{nap_id}{Character. The ID of the NAP}
+#'   \item{country_name}{Character. The name of the country}
+#'   \item{region}{Character. The region of the country}
+#'   \item{ldc_sids_marker}{Character. Whether the country is an LDC or SIDS}
+#'   \item{nap_lang}{Character. The languages available for the NAP}
+#'   \item{date_posted}{Character. The date the NAP was posted}
+#'   \item{pdf_link}{Character. The URL to the English PDF}
+#'   \item{pdf_path}{Character. The path to the downloaded file (during processing)}
+#'   \item{pdf_download_success}{Logical. Whether the download was successful}
+#'   \item{pdf_pages}{Integer. The number of pages in the PDF}
+#'   \item{pdf_text}{Character. The full text content extracted from the PDF}
+#' }
+#'
+#' @source \url{https://napcentral.org/submitted-naps}
+#' @note Last updated: See the package DESCRIPTION file or GitHub commit history
+"nap_data"
---
+++ R/napunfccc.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom dplyr add_row mutate
+#' @importFrom httr GET status_code user_agent write_disk
+#' @importFrom polite bow scrape
+#' @importFrom rvest html_attr html_nodes html_text
+#' @importFrom tibble tibble
+NULL
